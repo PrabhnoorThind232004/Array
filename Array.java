54bd8f2fb8219807a249832b40077ce77c4bf5c3
PrabhnoorThind232004 <166057799+PrabhnoorThind232004@users.noreply.github.com>
2024-04-04 15:14:11 -0400

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,13 @@
+package com.example.array;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            String[] Fruits= {"Mango","Pineapple","Gauva","Civi","Orange","Apple"};
+            
+            System.out.println("Fruit name is" + Fruits[3]);
+            System.out.println("Length of the array" + Fruits.length);
+	}
+
+}
